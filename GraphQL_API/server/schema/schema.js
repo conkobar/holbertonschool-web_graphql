76dd958f70ab318e0a67ce97b1baca1a3687b821
conkobar <connorkleine@gmail.com>
2023-07-19 11:45:49 -0500

    task 5

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -107,6 +107,18 @@ const RootQuery = new GraphQLObjectType({
         return _.find(projects, { id: args.id });
       },
     },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args) {
+        return tasks;
+      },
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
   }),
 });
 
